0412bae86ab001002a7bf5c181b175148a29b7b6
Tomasz <tom3k021@wp.pl>
2021-04-17 12:32:25 +0200

    Dodano wstępną walidację przy rejestracji.

--- app/src/main/java/com/example/rozwoznikv2/MainActivity.java
+++ app/src/main/java/com/example/rozwoznikv2/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.rozwoznikv2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Button goToRegBtn = new Button()
+
+        EditText emailEt = (EditText) findViewById(R.id.et_email_login);
+
+
+        Button goToRegistration = (Button) findViewById(R.id.btn_goToRegistration_login);
+        goToRegistration.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),registration_activity.class);
+                startActivity(intent);
+
+               final String email = emailEt.getText().toString();
+
+                if(email.length()==0)
+                {
+                    emailEt.requestFocus();
+                    emailEt.setError("Pole nie może być puste");
+                }
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/rozwoznikv2/registration_activity.java
+++ app/src/main/java/com/example/rozwoznikv2/registration_activity.java
@@ -2,13 +2,62 @@ package com.example.rozwoznikv2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class registration_activity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_activity);
-    }
+
+
+        EditText nameEt = (EditText) findViewById(R.id.edit_name_registration);
+        EditText phoneEt = (EditText) findViewById(R.id.edit_phoneNumber_registration);
+        EditText emailEt = (EditText) findViewById(R.id.edit_email_registration);
+        EditText passwordEt = (EditText) findViewById(R.id.edit_password_registration);
+        EditText confirmPassdEt = (EditText) findViewById(R.id.edit_confirmPass_registration);
+
+        List<EditText> editTextLIist = new ArrayList<EditText>();
+        editTextLIist.add(nameEt);
+        editTextLIist.add(phoneEt);
+        editTextLIist.add(emailEt);
+        editTextLIist.add(passwordEt);
+        editTextLIist.add(confirmPassdEt);
+        
+
+        Button  confirmRegistrationBtn = (Button) findViewById(R.id.btn_confirmRegistration_registration);
+       confirmRegistrationBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+                for (EditText el:editTextLIist
+                     ) {
+                    final String elString = el.getText().toString();
+                    if(elString.length()==0)
+                    {
+                        el.requestFocus();
+                        el.setError("Pole nie może być puste");
+                    }
+
+                }
+
+
+
+
+
+            }
+
+    });
+}
+
+
 }
