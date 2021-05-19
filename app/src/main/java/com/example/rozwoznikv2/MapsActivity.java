7864ba6c91bba9628a286d3a296f4937cc715151
Tomasz <tom3k021@wp.pl>
2021-05-19 19:55:53 +0200

    Dodano mapy - marker, koło, geocoder

--- app/src/main/java/com/example/rozwoznikv2/MapsActivity.java
+++ app/src/main/java/com/example/rozwoznikv2/MapsActivity.java
@@ -2,18 +2,29 @@ package com.example.rozwoznikv2;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Geocoder geocoder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,24 +34,47 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+         geocoder = new Geocoder(this);
+
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        try {
+           List<Address> adr= geocoder.getFromLocationName("żagań",1);
+
+           if(adr.size()>0) {
+               Address address = adr.get(0);
+
+
+
+               LatLng custom = new LatLng(address.getLatitude(), address.getLongitude());
+
+               CircleOptions copt = new CircleOptions()
+                       .center(custom)
+                       .radius(10000)
+                       .strokeColor(Color.GREEN);
+               Circle circle = mMap.addCircle(copt);
+
+               mMap.addMarker(new MarkerOptions().position(custom).title("Marker in London"));
+               mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(custom, 1));
+
+
+               Toast.makeText(MapsActivity.this, address.toString(), Toast.LENGTH_LONG).show();
+           }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+       // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        Log.i("mapa ready:","SSS");
+
     }
 }
